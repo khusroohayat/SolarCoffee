f90dadd2293a84ab66805bf260435819be3fa064
khusroohayat <khusroo.hayat@gmail.com>
2021-06-18 07:39:07 +0500

    Add Models and Db Context in Data Project

---
+++ SolarCoffee.Data/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UodatedOn { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public CustomerAddress PrimaryAddress { get; set; }
+    }
+}
