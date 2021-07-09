c12cc824e09396e233312b155b87175fb89415c3
khusroo hayat <khusroo_jaffri@hotmail.com>
2021-07-09 12:54:03 +0500

    inventory service integrated

--- solarcoffee.frontend/src/views/Inventory.vue
+++ solarcoffee.frontend/src/views/Inventory.vue
@@ -1,66 +1,58 @@
 <template>
-    <div class="inventory-container">
-        <h1 id="InventoryTitle">
-        Inventory Dashboard
-        </h1>
-        <hr />
+  <div class="inventory-container">
+    <h1 id="InventoryTitle">Inventory Dashboard</h1>
+    <hr />
 
-        <div class="inventory-actions">
-            <solar-button @click.native="showNewProductModal" id="addNewBtn">
-                Add New Item
-            </solar-button>
-            <solar-button @click.native="showShipMentModal" id="recieveShipmentBtn">
-                Recieve Shipment
-            </solar-button>
-        </div>
-        <table id="inventoryTable" class="table">
-            <tr>
-                <th>Item</th>
-                <th>Quantity On-hand</th>
-                <th>Unit Price</th>
-                <th>Taxable</th>
-                <th>Delete</th>
-            </tr>
+    <div class="inventory-actions">
+      <solar-button @click.native="showNewProductModal" id="addNewBtn">
+        Add New Item
+      </solar-button>
+      <solar-button @click.native="showShipMentModal" id="recieveShipmentBtn">
+        Recieve Shipment
+      </solar-button>
+    </div>
+    <table id="inventoryTable" class="table">
+      <tr>
+        <th>Item</th>
+        <th>Quantity On-hand</th>
+        <th>Unit Price</th>
+        <th>Taxable</th>
+        <th>Delete</th>
+      </tr>
 
-            <tr v-for="item in inventory">
-                <td>
-                    {{ item.product.name }}
-                </td>
-                <td>
-                    {{ item.quantityOnHand }}
-                </td>
-                <td>
-                    {{ item.product.price | price }}
-                </td>
-                <td>
-                    <span v-if="item.product.isTaxable">
-                        Yes
-                    </span>
-                    <span v-else>
-                        No
-                    </span>
-                </td>
-                <td>
-                    <div>
-                        X
-                    </div>
-                </td>
-            </tr>
-        </table>
+      <tr v-for="item in inventory">
+        <td>
+          {{ item.product.name }}
+        </td>
+        <td>
+          {{ item.quantityOnHand }}
+        </td>
+        <td>
+          {{ item.product.price | price }}
+        </td>
+        <td>
+          <span v-if="item.product.isTaxable"> Yes </span>
+          <span v-else> No </span>
+        </td>
+        <td>
+          <div>X</div>
+        </td>
+      </tr>
+    </table>
 
-        <new-product-modal 
-            v-if="isNewProductVisible" 
-            @save:product="saveNewProduct"
-            @close="closeModals"
-        />
+    <new-product-modal
+      v-if="isNewProductVisible"
+      @save:product="saveNewProduct"
+      @close="closeModals"
+    />
 
-        <shipment-modal
+    <shipment-modal
       v-if="isShipmentVisible"
       :inventory="inventory"
       @save:shipment="saveNewShipment"
       @close="closeModals"
     />
-    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -70,71 +62,47 @@ import SolarButton from "@/components/SolarButton.vue";
 import NewProductModal from "@/components/modals/NewProductModal.vue";
 import ShipmentModal from "@/components/modals/ShipmentModal.vue";
 import { IShipment } from "@/types/Shipment";
+import { InventoryService } from "@/services/inventory-service";
+
+const inventoryService = new InventoryService();
+
 @Options({
-    name: "Inventory",
+  name: "Inventory",
   components: { SolarButton, NewProductModal, ShipmentModal },
 })
 export default class Inventory extends Vue {
-    isNewProductVisible = false;
-    isShipmentVisible = false;
-    inventory: IProductInventory[] = [
-        {
-            id: 1,
-            product: {
-                id: 1,
-                name: "Some Product",
-                description: "Good Stuff",
-                price: 100,
-                createdOn: new Date(),
-                updatedOn: new Date(),
-                isTaxable: true,
-                isArchived: false
-            },
-            quantityOnHand: 100,
-            idealQuantity: 100
-        },
-        {
-            id: 2,
-            product: {
-                id: 2,
-                name: "Another Product",
-                description: "Good Stuff",
-                price: 100,
-                createdOn: new Date(),
-                updatedOn: new Date(),
-                isTaxable: false,
-                isArchived: false
-            },
-            quantityOnHand: 40,
-            idealQuantity: 50
-        }
-    ];
+  isNewProductVisible = false;
+  isShipmentVisible = false;
+  inventory: IProductInventory[] = [];
 
-    closeModals() {
-        this.isShipmentVisible = false;
-        this.isNewProductVisible = false;
-    }
+  closeModals() {
+    this.isShipmentVisible = false;
+    this.isNewProductVisible = false;
+  }
 
-    showNewProductModal() {
-        this.isNewProductVisible = true;
-    }
+  showNewProductModal() {
+    this.isNewProductVisible = true;
+  }
 
-    showShipMentModal() {
-        this.isShipmentVisible = true;
-    }
+  showShipMentModal() {
+    this.isShipmentVisible = true;
+  }
 
-    saveNewProduct(newProduct: IProduct) {
-        console.log("SaveNewProduct");
-        console.log(newProduct);
-        
-        
-    }
+  saveNewProduct(newProduct: IProduct) {
+    console.log("SaveNewProduct");
+    console.log(newProduct);
+  }
 
-    saveNewShipment(shipment: IShipment) {
-        console.log('saveNewShipment:');
-        console.log(shipment);
-    }
+  saveNewShipment(shipment: IShipment) {
+    console.log("saveNewShipment:");
+    console.log(shipment);
+  }
 
-    
+  async initialize() {
+    this.inventory = await inventoryService.getInventory();
+  }
+  async created() {
+    await this.initialize();
+  }
 }
 </script>
